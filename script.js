fed668614ea71c53a78631065bbbcf2821bdb7ea
stefnotch <brandmairstefan@gmail.com>
2016-02-10 18:06:58 +0000

    Working triangle

--- script.js
+++ script.js
@@ -11,17 +11,13 @@ function start() {
   //init the WebGL
   var glcanvas = document.getElementById("glcanvas");
   gl = initWebGL(glcanvas);
-
+  
   if (gl) {
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Enable depth testing
-    gl.enable(gl.DEPTH_TEST);
-    // Near things obscure far things
-    gl.depthFunc(gl.LEQUAL);
-    // Clear the color as well as the depth buffer.
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    var buffer = createVBO([
+      0.0, 0.5,
+      0.5, -0.5, -0.5, -0.5,
+    ]);
     var vertexShader = createShader(`
     attribute vec2 coordinates;
     void main(void){
@@ -35,7 +31,6 @@ function start() {
 
     // Put the vertex shader and fragment shader together into
     // a complete program
-    //
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
@@ -43,21 +38,38 @@ function start() {
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
       throw new Error(gl.getProgramInfoLog(shaderProgram));
 
+    // Everything we need has now been copied to the graphics
+    // hardware, so we can start drawing
 
+    // Clear the drawing surface
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Tell WebGL which shader program to use
+    gl.useProgram(shaderProgram);
+
+
+    // Tell WebGL that the data from the array of triangle
+    // coordinates that we've already copied to the graphics
+    // hardware should be fed to the vertex shader as the
+    // parameter "coordinates"
+    var coordinatesVar = gl.getAttribLocation(shaderProgram, "coordinates");
+    gl.enableVertexAttribArray(coordinatesVar);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(coordinatesVar, 2, gl.FLOAT, false, 0, 0);
+    
+    //Triangle
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 }
 
-function createVBO() {
+function createVBO(vertices) {
   // Copy an array of data points forming a triangle to the
   // graphics hardware
-  //
-  var vertices = [
-    0.0, 0.5,
-    0.5, -0.5, -0.5, -0.5,
-  ];
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  return buffer;
 }
 
 function createShader(shaderCode, shaderType) {
