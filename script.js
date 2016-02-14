73da75cb1a5f2303bdcaece07a37b2824def2e0e
stefnotch <brandmairstefan@gmail.com>
2016-02-14 12:57:18 +0000

    requestAnimationFrame, keyboard, translation, scale

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ Translation tutorial
 Redraw tutorial
 */
 var gl; //WebGL lives in here!
-
+var offsetLoc, pos = [0,0,0,0], velocity=[0,0,0,0];
 //Called by the body
 function start() {
   //init the WebGL
@@ -20,20 +20,43 @@ function start() {
     glcanvas.height = window.innerHeight;
     redraw();
   }, false);
-
+  window.addEventListener("keydown", keyboardHandlerDown);
+  window.addEventListener("keyup", keyboardHandlerUp);
   gl = initWebGL(glcanvas);
 
   if (gl) {
 
     var buffer = createVBO([
-      0.0, 0.5, 0,
-      0.5, -0.5, 0, -0.5, -0.5, 0
+      // left column
+      0, 0, 0,
+      30, 0, 0,
+      0, 150, 0,
+      0, 150, 0,
+      30, 0, 0,
+      30, 150, 0,
+
+      // top rung
+      30, 0, 0,
+      100, 0, 0,
+      30, 30, 0,
+      30, 30, 0,
+      100, 0, 0,
+      100, 30, 0,
+
+      // middle rung
+      30, 60, 0,
+      67, 60, 0,
+      30, 90, 0,
+      30, 90, 0,
+      67, 60, 0,
+      67, 90, 0
     ]);
     var vertexShader = createShader(`
     attribute vec4 coordinates;
     uniform vec4 offset; 
     void main(void){
-      gl_Position = coordinates + offset;
+    vec4 pos = vec4(coordinates.x*0.003,coordinates.y*-0.005,coordinates.z,coordinates.w) + offset;
+      gl_Position = pos;
     }`, gl.VERTEX_SHADER);
 
     var fragmentShader = createShader(`
@@ -55,24 +78,27 @@ function start() {
 
     // Clear the drawing surface
     gl.clearColor(1.0, 1.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     // Tell WebGL which shader program to use
     gl.useProgram(shaderProgram);
     //Feed the shader
     feedShader(shaderProgram, buffer, "coordinates");
     //Change a uniform variable
-    var offsetLoc = gl.getUniformLocation(shaderProgram, "offset");
-    gl.uniform4fv(offsetLoc, [-0.5, 0, 0, 0]); // offset it to the right half the screen
-    redraw();
+    offsetLoc = gl.getUniformLocation(shaderProgram, "offset");
+    
 
+    window.requestAnimationFrame(redraw);
   }
 }
 
 function redraw() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  pos[0] += velocity[0];
+  pos[1] += velocity[1];
+  pos[2] += velocity[2];
+  gl.uniform4fv(offsetLoc, pos);
   //Triangle
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-
+  gl.drawArrays(gl.TRIANGLES, 0, 18);
+  window.requestAnimationFrame(redraw);
 }
 
 function createVBO(vertices) {
@@ -136,4 +162,25 @@ function initWebGL(canvas) {
   }
 
   return gl;
+}
+
+function keyboardHandlerDown(keyboardEvent) {
+  switch (keyboardEvent.key) {
+    case "ArrowUp":
+      velocity[1] = 0.01;
+      break;
+    case "ArrowDown":
+      velocity[1] = -0.01;
+      break;
+    case "ArrowLeft":
+      velocity[0] = -0.01;
+      break;
+    case "ArrowRight":
+      velocity[0] = 0.01;
+      break;
+  }
+}
+
+function keyboardHandlerUp(keyboardEvent) {
+
 }
